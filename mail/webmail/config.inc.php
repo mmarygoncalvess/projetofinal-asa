94785d1fd95a842926953d9aafbacab69e57b3b0
mmarygoncalvess <161530981+mmarygoncalvess@users.noreply.github.com>
2024-08-27 00:04:30 +0000

    codigo

---
+++ mail/webmail/config.inc.php
@@ -0,0 +1,4 @@
+<?php
+$config['db_dsnw'] = 'sqlite:////var/roundcube/db/sqlite.db';
+$config['default_host'] = 'mail.ifrn.asa.br';
+$config['smtp_server'] = 'mail.ifrn.asa.br';
---
+++ web/wordpress/wp-config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', '192.168.56.1'); // Ajuste aqui se necessário
